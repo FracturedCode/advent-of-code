836a941dd425739b75e9efb59238274332b63fd0
Gabe C <gabe@fracturedcode.net>
2023-12-14 22:51:48 -0700

    WIP day 2022 day 12

---
+++ advent_2022/advent_2022/Day12.cs
@@ -0,0 +1,20 @@
+using BenchmarkDotNet.Attributes;
+
+namespace advent_2022;
+
+public class Day12 : AdventDay
+{
+	public Day12() : base(nameof(Day12)) { }
+
+	[Test(ExpectedResult = 31)]
+	[Benchmark]
+	public override object Part1()
+	{
+		throw new NotImplementedException();
+	}
+
+	public override object Part2()
+	{
+		throw new NotImplementedException();
+	}
+}
