e83cbc807d24fe372905cd07edf4aba671b165e8
Jesse C <jesse@fracturedcode.net>
2025-12-07 17:21:00 -0700

    2025 Day 1

--- advent_2025/Day1.py
+++ advent_2025/Day1.py
@@ -1,6 +1,9 @@
+from aocd import get_data
+
 from functools import reduce
 from typing import NamedTuple
 from enum import StrEnum
+import math
 
 class Direction(StrEnum):
     LEFT = 'L'
@@ -10,28 +13,47 @@ class Rotation(NamedTuple):
     direction: Direction
     distance: int
 
-zero_count = 0
+class PuzzleState(NamedTuple):
+    dial_position: int
+    zero_stop_count: int
+    zero_seen_count: int
+
 dial_size = 100
 
-def rotate(position: int, rotation: Rotation):
-    global zero_count, dial_size
+def rotate(state: PuzzleState, rotation: Rotation):
+    global dial_size
 
-    if rotation.direction == Direction.LEFT:
-        position = (position - rotation.distance) % dial_size
-    elif rotation.direction == Direction.RIGHT:
-        position = position + rotation.distance
+    pos = state.dial_position + rotation.distance * (-1 if rotation.direction == Direction.LEFT else 1)
 
-    if position == 0:
-        zero_count += 1
-    return position
+    zero_stopped = pos % dial_size == 0
 
-with open("./inputs/Day1.txt") as f:
-    lines = f.readlines()
+    rotation_zero_seen_count = abs(pos // dial_size - state.dial_position // dial_size)
+    rotation_zero_seen_count += rotation.direction == Direction.LEFT and zero_stopped
+    # don't double count when we were already stopped on zero
+    rotation_zero_seen_count -= rotation.direction == Direction.LEFT and state.dial_position % dial_size == 0
 
-lines = [x.strip() for x in lines]
-lines = [x for x in lines if len(x) > 0]
+    state = PuzzleState(
+        pos,
+        state.zero_stop_count + zero_stopped,
+        state.zero_seen_count + rotation_zero_seen_count)
+    #print(state)
+    return state
 
-lines = map(lambda x: Rotation(Direction(x[0]), int(x[1:])), lines)
+example_data = """
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+"""
+data = get_data(day=1, year=2025).strip().splitlines()
+#data = example_data.strip().splitlines()
+data = map(lambda x: Rotation(Direction(x[0]), int(x[1:])), data)
 
-reduce(rotate, lines, 50)
-print(zero_count)
+puzzle_state = reduce(rotate, data, PuzzleState(50, 0, 0))
+print(puzzle_state)
